4b688a5513083730ae6290e4b6187756a251235a
tetsuya-azami <zqianjian35@gmail.com>
2025-01-24 09:56:27 +0900

    learn data structure

--- lesson.go
+++ lesson.go
@@ -13,10 +13,12 @@ func init() {
 func main() {
 	bazz()
 	var i int = 1
-	var t, f bool = true, false
+	t, f := true, false
 	fmt.Println("Hello world", "golang", "\n", time.Now(), "\n", "int: ", i, "\n", "bool: ", t, f)
 	fmt.Println(user.Current())
 	printTypeDefaults()
+	array()
+	slice()
 }
 
 func bazz() {
@@ -31,4 +33,23 @@ func printTypeDefaults() {
 		t, f bool
 	)
 	fmt.Printf("型のデフォルト値: %v %v %q %v %v\n", i, f64, s, t, f)
+}
+
+func array() {
+	var a [2]int
+	a[0] = 100
+	a[1] = 200
+	fmt.Println(a) // [100 200]
+
+	b := [2]int{100} // 要素が足りない場合はデフォルト値(この場合は0)が入る
+	fmt.Println(b)   // [100 0]
+}
+
+func slice() {
+	a := []int{1, 2, 3}
+	fmt.Println(a) // [1 2 3]
+	a = append(a, 4)
+	fmt.Println(a) // [1 2 3 4]
+	a[2] = 100
+	fmt.Println(a) // [1 2 100 4]
 }
