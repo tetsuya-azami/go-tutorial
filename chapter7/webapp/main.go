a540c3439844aea31abca9126106af4885761e9b
tetsuya-azami <zqianjian35@gmail.com>
2025-01-30 09:38:00 +0900

    learn create  webapp

---
+++ chapter7/webapp/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"text/template"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	return os.WriteFile(p.Title+".txt", p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	body, err := os.ReadFile(title + ".txt")
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	// GET /view/test → render view.html
+	(&Page{"test", []byte("This is a sample Page.")}).save()
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		renderTemplate(w, "edit", &Page{Title: title})
+		return
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := Page{Title: title, Body: []byte(body)}
+
+	err := p.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func renderTemplate(w http.ResponseWriter, view string, p *Page) {
+	t, err := template.ParseFiles(view + ".html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, p)
+}
